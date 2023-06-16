4dc08b57ebe1af89e69ca4498ff5227378e47f94
Gary Polhill <gary.polhill@hutton.ac.uk>
2023-06-16 17:34:16 +0100

    debugging choosers

--- nlogo.py
+++ nlogo.py
@@ -1747,6 +1747,8 @@ class Sample:
         # TO-DO: update this to handle 'minimum' == 'one-of'
         if isinstance(self.param, Chooser):
             print("minimum is \"" + minimum + "\", with type " + str(type(minimum)) + "\n")
+            for x in param.choices:
+                print("choice \"" + x + "\" has type " + str(type(x)))
             if minimum in param.choices:
                 self.minimum = param.choices.index(minimum)
             elif '"' + minimum + '"' in param.choices:
